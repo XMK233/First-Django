25c3826150f947b557d2c6b39b9d751031aded26
Progroveriman <clxfxmk@qq.com>
2017-03-22 12:23:17 +0800

    Fisrt edition
    have no Csrf function

---
+++ mysite5/urls.py
@@ -0,0 +1,27 @@
+"""mysite5 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from online import views as ov
+urlpatterns = [
+    url(r'^online/$', ov.login, name='login'),
+    url(r'^online/login/$',ov.login,name = 'login'),
+    url(r'^online/regist/$',ov.regist,name = 'regist'),
+    url(r'^online/index/$',ov.index,name = 'index'),
+    url(r'^online/logout/$',ov.logout,name = 'logout'),
+
+    url(r'^admin/', admin.site.urls),
+]
